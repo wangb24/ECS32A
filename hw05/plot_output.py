27a4181201f139b6b4da9db6108d49086a964811
wangb24 <dev@h.wb233.moe>
2023-05-19 21:36:22 -0700

    Modified some code

--- hw05/plot_output.py
+++ hw05/plot_output.py
@@ -9,17 +9,26 @@ This program plots a line chart of the output of the 'moving_ave_csv.py' program
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from first_ave import running_average
+from read_csv import read_csv
+
+# Set constants
+K = 18  # Window size
 
 sns.set_theme() # Set the theme for the plot
 
-# Read the data
-dat = pd.read_csv('MovingAve.csv')
-# print(dat.head())
+# dat = pd.read_csv('MovingAve.csv')
+# Create the data
+yrs, val = read_csv('data/SacramentoTemps.csv')
+val, yrs = running_average(val, yrs, K)
+dat = pd.DataFrame({'Year': yrs, 'Value': val})
+del yrs, val
 
 # Plot the data
 plot = sns.lineplot(data = dat, x = "Year", y = "Value")
-plot.set_title("Sacramento July Temperatures (Moving Average, k = 18)")
+plot.set_title(f"Sacramento July Temperatures (Moving Average, k = {K})")
 plot.set_xlabel("Year")
 plot.set_ylabel("Temperature Anomaly")
 
+# plt.show()
 plt.savefig("figure/Plot_Part_7.png", dpi = 900)
